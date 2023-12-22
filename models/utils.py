86100895705c5b9e140a06612eb05e74f5f41995
unasthana <128328811+unasthana@users.noreply.github.com>
2023-12-22 13:44:00 -0500

    Added docstrings

--- models/utils.py
+++ models/utils.py
@@ -1,2 +1,16 @@
+"""
+Utility function for counting the number of trainable parameters in a PyTorch model.
+
+"""
+
 def count_parameters(model):
+    """
+    Count the number of trainable parameters in a PyTorch model.
+
+    Parameters:
+    - model: PyTorch model.
+
+    Returns:
+    - int: Number of trainable parameters.
+    """
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
