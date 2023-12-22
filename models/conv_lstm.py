922e4354aadf22d671dd487ecf63a405bfae81cf
unasthana <128328811+unasthana@users.noreply.github.com>
2023-12-22 13:42:26 -0500

    Added docstrings

--- models/conv_lstm.py
+++ models/conv_lstm.py
@@ -1,11 +1,46 @@
+"""
+Module containing the ConvLSTM class for sequence modeling.
+
+This module defines the ConvLSTM class, which is a combination of Convolutional
+and Long Short-Term Memory (LSTM) layers for sequence modeling.
+
+"""
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 class ConvLSTM(nn.Module):
+    """
+    ConvLSTM module for sequence modeling.
+
+    Attributes:
+    - num_feats (int): Number of input features.
+    - conv_kernel_size (int): Size of the convolutional kernel.
+    - embedding_size (int): Size of the embedding.
+    - num_layers (int): Number of LSTM layers.
+    - dropout (float): Dropout rate.
+    - conv1 (nn.Conv1d): Convolutional layer.
+    - relu1 (nn.ReLU): ReLU activation layer.
+    - pool (nn.MaxPool1d): Max pooling layer.
+    - lstm (nn.LSTM): LSTM layer.
+    - ln (nn.LayerNorm): Layer normalization layer.
+    - o_proj (nn.Linear): Output projection layer.
+    - sigmoid (nn.Sigmoid): Sigmoid activation layer.
+    """
 
     def __init__(self, num_feats, conv_kernel_size, embedding_size, num_layers,
         dropout=0.0):
+        """
+        Initialize the ConvLSTM module.
+
+        Parameters:
+        - num_feats (int): Number of input features.
+        - conv_kernel_size (int): Size of the convolutional kernel.
+        - embedding_size (int): Size of the embedding.
+        - num_layers (int): Number of LSTM layers.
+        - dropout (float): Dropout rate.
+        """
      
         super(ConvLSTM, self).__init__()
 
@@ -31,6 +66,15 @@ class ConvLSTM(nn.Module):
         self.sigmoid = nn.Sigmoid()
     
     def forward(self, x):
+        """
+        Forward pass of the ConvLSTM module.
+
+        Parameters:
+        - x (torch.Tensor): Input tensor.
+
+        Returns:
+        - torch.Tensor: Output tensor.
+        """
 
         y = torch.permute(x, [0, 2, 1]) 
         y = F.pad(y, (self.conv_kernel_size,0), 'replicate') 
